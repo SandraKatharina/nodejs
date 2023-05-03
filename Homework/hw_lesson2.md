10f934f7dbcafc90bb8e0503a34013f660def08f
Jocee Paul <joceepaul@me.com>
2023-05-03 14:49:39 +0200

    create folder lesson2 and files for hw and work on make paginated fetch on button click

---
+++ Homework/hw_lesson2.md
@@ -0,0 +1,4 @@
+Add pagination to your Pokemon List
+
+Only show 20 pokemon at a time.
+Have a Previous and Next button to replace the <ol> content with the corresponding pokemon
---
+++ lesson2/e1-remember_arrays.js
@@ -0,0 +1,25 @@
+// const numbers = [23, 23, 2, 5, 3, 12, 80, 2, 5, -12, 23, -90, 30];
+//         const bigNumbers = [];
+//         for (let i = 0; i < numbers.length; i++) {
+//             if (numbers[i] > 20) {
+//                 bigNumbers.push(numbers[i]);
+//             }
+//         }
+//         console.log(bigNumbers);
+
+const coolPeople = [
+  { name: "Helder", age: 42 },
+  { name: "Hugo", age: 52 },
+  {
+    name: "Sandra",
+    age: 44,
+  },
+  { name: "Bruno", age: 41 },
+];
+
+const ageEvenNumber = [];
+
+for (let i = 0; i < coolPeople.length; i++) {
+  if (coolPeople[i].age % 2 === 0);
+}
+console.log(coolPeople.age);
